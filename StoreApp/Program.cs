137bab80766b83675a89b6b73001d7c27fc2e90d
SolihCoder <BilolMuhammadSolih@gmail.com>
2025-05-23 19:36:43 +0500

    Added web socket api endpoint at /chat

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,4 +1,7 @@
 using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using StoreApp;
 using StoreApp.Core;
@@ -10,16 +13,14 @@ using StoreApp.Features.Notifications;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(
-  options =>
+builder.WebHost.ConfigureKestrel(options =>
   {
     options.Limits.MaxRequestBodySize = int.MaxValue;
     options.Listen(IPAddress.Parse("0.0.0.0"), 8888);
   }
 );
 
-builder.Services.AddCors(
-  options =>
+builder.Services.AddCors(options =>
   {
     options.AddPolicy(
       "AllowAll",
@@ -41,8 +42,7 @@ builder.Services.AddSqlite<StoreDbContext>(builder.Configuration.GetConnectionSt
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
 
 
-builder.Services.AddSwaggerGen(
-  options =>
+builder.Services.AddSwaggerGen(options =>
   {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "StoreApp API", Version = "v1" });
 
@@ -93,6 +93,9 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
+app.UseWebSockets();
+app.UseRouting();
+
 var uploadsDir = Path.Combine(builder.Environment.ContentRootPath, "uploads");
 
 if (!Directory.Exists(uploadsDir))
@@ -117,6 +120,71 @@ app.UseAuthorization();
 
 
 app.MapControllers();
+app.Map(
+  "/chat",
+  async context =>
+  {
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+      using var websocket = await context.WebSockets.AcceptWebSocketAsync();
+      await Echo(websocket);
+    }
+    else
+    {
+      context.Response.StatusCode = 400;
+    }
+  }
+);
 
+async Task Echo(WebSocket webSocket)
+{
+  var buffer = new byte[4 * 2048];
+  WebSocketReceiveResult result = await webSocket.ReceiveAsync(
+    new ArraySegment<byte>(buffer),
+    CancellationToken.None
+  );
+  
+  var jsonString = Encoding.UTF8.GetString(buffer, 0, result.Count);
+  var map = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
+  map["direction"] = "to";
+
+  var response = JsonSerializer.Serialize(map);
+  var responseBytes = Encoding.UTF8.GetBytes(response);
+
+  await webSocket.SendAsync(
+    new ArraySegment<byte>(responseBytes, 0, responseBytes.Length),
+    result.MessageType,
+    result.EndOfMessage,
+    CancellationToken.None
+  );
+
+  while (!result.CloseStatus.HasValue)
+  {
+    result = await webSocket.ReceiveAsync(
+      new ArraySegment<byte>(buffer),
+      CancellationToken.None
+    );
+
+    jsonString = Encoding.UTF8.GetString(buffer, 0, result.Count);
+    map = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
+    map["direction"] = "to";
+
+    response = JsonSerializer.Serialize(map);
+    responseBytes = Encoding.UTF8.GetBytes(response);
+
+    await webSocket.SendAsync(
+      new ArraySegment<byte>(responseBytes, 0, responseBytes.Length),
+      result.MessageType,
+      result.EndOfMessage,
+      CancellationToken.None
+    );
+  }
+
+  await webSocket.CloseAsync(
+    result.CloseStatus.Value,
+    result.CloseStatusDescription,
+    CancellationToken.None
+  );
+}
 
 app.Run();
